465c8ae0a8f67b6f35990eacc51fabd646c7adc5
CHARLEY CARRIERO <carriero.char@gmail.com>
2021-06-10 08:55:18 -0400

    added basic component structure and template header

---
+++ src/components/Header/header.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import * as styles from "./header.module.css";
+
+const Header = () => {
+  return <div className={styles.container}>I will be a header</div>;
+};
+
+export default Header;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import * as React from 'react'
+import Header from '../components/Header/header'
 
 
 const IndexPage = () => {
   return (
     <main>
+      <Header/>
       <title>Home Page</title>
       <h1>hello world!</h1>
       <p>this is my new app</p>
