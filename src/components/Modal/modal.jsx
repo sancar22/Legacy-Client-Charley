db91ca186f17547b64237ea0377b2cef24a5f208
CHARLEY CARRIERO <carriero.char@gmail.com>
2021-06-11 13:33:29 -0400

    modal base is complete

--- src/components/Modal/modal.jsx
+++ src/components/Modal/modal.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import * as styles from "./modal.module.css";
 
 const Modal = ({ show, handleClose }) => {
-  return <div>modal</div>;
+
+
+  return (
+    <div className={show ? styles.modalShow : styles.modalHide}>
+      <div className={styles.modalMain}>
+        <p className={styles.headerText}>Add your url</p>
+        <form className={styles.form}>
+          <input className={styles.input}></input>
+          <button>submit</button>
+        </form>
+        <button onClick={handleClose} className={styles.close}>
+          x
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Modal;
--- src/components/RecipeAdder/recipeAdder.jsx
+++ src/components/RecipeAdder/recipeAdder.jsx
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import Modal from "../Modal/modal";
 import * as styles from "./recipeAdder.module.css";
 
 const RecipeAdder = () => {
+  const [modalStatus, setModalStatus] = useState(false);
+
+
+  const handleModal = () => {
+    setModalStatus(!modalStatus);
+  };
+
   return (
     <div className={styles.container}>
-      <div className={styles.addButton}>+</div>
+      <div className={styles.addButton} onClick={handleModal}>
+        +
+
+      </div>
+      <Modal show={modalStatus} handleClose={handleModal}></Modal>
     </div>
   );
 };
