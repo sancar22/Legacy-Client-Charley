8807e9473ed8456aacc43c7d13c4b09825b82360
CHARLEY CARRIERO <carriero.char@gmail.com>
2021-06-10 22:27:10 -0400

    more

--- src/state/store.js
+++ src/state/store.js
@@ -3,15 +3,17 @@ import { persistStore, persistReducer } from 'redux-persist';
 import {composeWithDevTools} from 'redux-devtools-extension';
 import { createLogger } from 'redux-logger'
 import storage from 'redux-persist/lib/storage';
-import reducers from './reducers';
+import rootReducer from './reducers';
 
 const persistConfig = {
   key: 'root',
   storage,
-  whitelist: ['isAuthenticated', 'username', 'recipeStore'],
-  blacklist: ['_persist'],
+  //whitelist: ['isAuthenticated', 'username', 'recipeStore'],
+  //blacklist: ['_persist'],
+  timeout: null,
 };
-const persistedReducer = persistReducer(persistConfig, reducers);
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const loggerMiddleware = createLogger();
 const middleware = composeWithDevTools(applyMiddleware(loggerMiddleware));
