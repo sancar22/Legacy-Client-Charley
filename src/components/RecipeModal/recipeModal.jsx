3f0baddea01a66b1c54eb90382997b84a98d6b82
CHARLEY CARRIERO <carriero.char@gmail.com>
2021-06-12 13:52:59 -0400

    finished formatting large modal

--- src/components/RecipeModal/recipeModal.jsx
+++ src/components/RecipeModal/recipeModal.jsx
@@ -4,7 +4,25 @@ import * as styles from "./recipeModal.module.css";
 const RecipeModal = ({ show, handleClose, recipe }) => {
   return (
     <div className={show ? styles.modalShow : styles.modalHide}>
-      <div onClick={handleClose}>x</div>
+      <div onClick={handleClose} className={styles.closeIcon}>
+        x
+      </div>
+      <div className={styles.title}>{recipe.name}</div>
+
+      <div className={styles.content}>
+        <div className={styles.recipeYield}>{recipe.recipeYield}</div>
+        <ul>
+          {recipe.recipeIngredient.map((ingredient) => (
+            <li className={styles.ingredient}>{ingredient}</li>
+          ))}
+        </ul>
+        <div className={styles.prepareHeader}>Prepare</div>
+        <ul>
+          {recipe.recipeInstructions.map((ingredient) => (
+            <li className={styles.ingredient}>{ingredient}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
