5f3d68234e7ddc2c789e30a2b2fa07b4d95d770f
CHARLEY CARRIERO <carriero.char@gmail.com>
2021-06-15 13:05:52 -0400

    fixed navigator abuse in nav bar

--- src/components/Headings/NavBar/navbar.jsx
+++ src/components/Headings/NavBar/navbar.jsx
@@ -1,30 +1,24 @@
-import { navigate } from "gatsby";
+import { Link } from "gatsby";
 import React from "react";
 import * as styles from "./navbar.module.css";
 
 const NavBar = () => {
   return (
     <div className={styles.container}>
-      <div
-        className={styles.navElement}
-        onClick={() => navigate("/recipes")}
-        aria-hidden="true"
-      >
-        my recipes
+      <div className={styles.navElement}>
+        <Link className={styles.text} to="/recipes">
+          my recipes
+        </Link>
       </div>
-      <div
-        className={styles.navElement}
-        onClick={() => navigate("/profile")}
-        aria-hidden="true"
-      >
-        home
+      <div className={styles.navElement}>
+        <Link className={styles.text} to="/profile">
+          home
+        </Link>
       </div>
-      <div
-        className={styles.navElement}
-        onClick={() => navigate("/visitFriends")}
-        aria-hidden="true"
-      >
-        visit friends
+      <div className={styles.navElement}>
+        <Link className={styles.text} to="/visitFriends">
+          visit friends
+        </Link>
       </div>
     </div>
   );
