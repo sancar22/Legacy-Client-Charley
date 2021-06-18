094d3e0653a6b6265e898fe7ec042effb3030b38
Ujwal Kandukuri <ujwal.kanduk@gmail.com>
2021-06-18 19:37:40 -0400

    navbar to ts

---
+++ src/components/Headings/NavBar/navbar.tsx
@@ -0,0 +1,25 @@
+import { Link } from 'gatsby';
+import React from 'react';
+import * as styles from './navbar.module.css';
+
+const NavBar = (): JSX.Element => (
+  <div className={styles.container}>
+    <div className={styles.navElement}>
+      <Link className={styles.text} to='/recipes'>
+        my recipes
+      </Link>
+    </div>
+    <div className={styles.navElement}>
+      <Link className={styles.text} to='/profile'>
+        home
+      </Link>
+    </div>
+    <div className={styles.navElement}>
+      <Link className={styles.text} to='/visitFriends'>
+        visit friends
+      </Link>
+    </div>
+  </div>
+);
+
+export default NavBar;
