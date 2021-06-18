4dbec1a7ba9a87ae4d0a86c18f0312afc071d76d
Ujwal Kandukuri <ujwal.kanduk@gmail.com>
2021-06-18 15:31:51 -0400

    replace tslint with eslint

---
+++ .eslintrc.js
@@ -0,0 +1,56 @@
+module.exports = {
+  globals: {
+    __PATH_PREFIX__: true,
+  },
+  env: {
+    es2021: true, // Use the latest EcmaScript features
+    node: true, // Expect Node as execution environment
+    'jest/globals': true, // We are using jest globals
+  },
+  extends: [
+    // Use Airbnb and typescript-eslint configuration
+    'airbnb-base',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser', // Parse code using @typescript-eslint/parser
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'import', 'jest'],
+  rules: {
+    camelcase: 0, // Backend frameworks and services use a lot of snake_case notation
+    'no-underscore-dangle': 0, // We might use underscore dangles (everyone uses them)
+    'import/extensions': 0, // We prefer import plugin not to check extensions as it's something @typescript-eslint will do
+    'no-shadow': 0, // We prefer eslint not to check variable shadowing as it's something @typescript-eslint will do (next line)
+    '@typescript-eslint/no-shadow': ['error'],
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }], // Here we let ourselves use `_` as unused function parameter
+    'jest/no-disabled-tests': 'warn',
+    'jest/no-focused-tests': 'error',
+    'jest/no-identical-title': 'error',
+    'jest/prefer-to-have-length': 'warn',
+    'jest/valid-expect': 'error',
+    'consistent-return': 0,
+    'no-console': 1,
+  },
+  overrides: [
+    // Overrides are rule for specific file extensions
+    {
+      files: ['**/*.ts', '**/*.tsx'],
+      rules: {
+        'import/prefer-default-export': 0, // We do not prefer default export in typescript
+      },
+    },
+  ],
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'], // Here we specify what extensions we let parser to resolve
+    },
+    'import/resolver': {
+      typescript: {
+        // And finally, here we ask ESLint to check if type definitions for the package installed
+        alwaysTryTypes: true,
+      },
+    },
+  },
+};
