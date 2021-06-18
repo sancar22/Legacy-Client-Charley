61ffd05be257f1dab8dcef9632ffedf97ff027f2
Ujwal Kandukuri <ujwal.kanduk@gmail.com>
2021-06-18 19:40:12 -0400

    loadingInd to ts

---
+++ src/components/LoadingInd/loadingInd.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { usePromiseTracker } from 'react-promise-tracker';
+import Loader from 'react-loader-spinner';
+import * as styles from './loadingInd.module.css';
+
+const LoadingInd = ({ color }): JSX.Element => {
+  const dotColor: string = color || 'white';
+  const { promiseInProgress } = usePromiseTracker();
+
+  return (
+    <>
+      {promiseInProgress && (
+        <div className={styles.loading__container}>
+          <Loader type='ThreeDots' color={dotColor} height='100' width='100' />
+        </div>
+      )}
+    </>
+  );
+};
+
+export default LoadingInd;
